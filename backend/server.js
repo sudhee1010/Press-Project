3cf323f456a0c66fb985ac2a70cfa088595187ea
sibilr1016 <sibilr1016@gmail.com>
2025-04-04 16:27:41 +0530

    Your changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+dotenv.config();
 const app = express();
+connectDB();
 
 app.listen(3000, () => console.log(`Server is running in port`));
