f0c76ea88d0f3f271042a3bd24ad747d430131a8
sudhee krishna <sudheekrishna100@gmail.com>
2025-04-21 15:17:48 +0530

    change in server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/database.js";
 import onlineCustomerRouter from "./route/onlineCustomerRoute.js";
 import printingUnitRouter from "./route/printingPressunitRoute.js";
 import orderRouter from "./route/OrderRoutes.js";
+import tokenRouter from "./route/tokenRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -15,12 +16,13 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use("/api/v1/onlineCustomer", onlineCustomerRouter);
-app.use("/api/v1/Order", orderRouter);
+app.use("/api/v1/order", orderRouter);
+app.use("/api/v1/printingUnit",printingUnitRouter );
+app.use("/api/v1/tokenRouter",tokenRouter );
 
 
 const PORT = process.env.PORT || 3000;  
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-app.use("/printingUnit",printingUnitRouter );
 
 
 export { app };
