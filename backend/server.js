3865fdaa037687901685b78c733d6985f4fddb4b
sudhee krishna <sudheekrishna100@gmail.com>
2025-04-21 15:02:46 +0530

    onlineCustomerupdate1

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,8 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use("/api/onlineCustomer", onlineCustomerRouter);
-
-// app.listen(3000, () => console.log(`Server is running in port`));
-const PORT = process.env.PORT || 3000;  // Use PORT from .env or default to 3000
+app.use("/api/v1/onlineCustomer", onlineCustomerRouter);
+const PORT = process.env.PORT || 3000;  
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 export { app };
