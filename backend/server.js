980c5fe1730a74f9f22a4bb7d8d7922c5197c92a
sibilr1016 <sibilr1016@gmail.com>
2025-04-22 16:53:51 +0530

    formatting changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from 'cors';
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/database.js";
 import onlineCustomerRouter from "./route/onlineCustomerRoute.js";
@@ -10,12 +10,14 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 app.use(cookieParser());
 app.use("/api/onlineCustomer", onlineCustomerRouter);
 
 // app.listen(3000, () => console.log(`Server is running in port`));
-const PORT = process.env.PORT || 3000;  // Use PORT from .env or default to 3000
+const PORT = process.env.PORT || 3000; // Use PORT from .env or default to 3000
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 export { app };
