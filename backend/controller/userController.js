098782ae5c85b8fbf7ab66b646be217277adc42b
sibilr1016 <sibilr1016@gmail.com>
2025-04-23 10:56:29 +0530

    user routes,controller,schema created

---
+++ backend/controller/userController.js
@@ -0,0 +1,38 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+// import generateToken from "../utils/generateToken.js";
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists)
+    return res.status(400).json({ message: "User already exists" });
+
+  const user = await User.create({ name, email, password, role });
+  // generateToken(res, user._id);
+  res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  });
+});
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    // generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
