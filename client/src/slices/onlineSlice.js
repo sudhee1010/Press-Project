1c09dd57932235fbfa2aa1d20999c4436058f3b5
Ashish-Gladson <ashish.benny23@gmail.com>
2025-05-13 13:35:03 +0530

    customerLogin

---
+++ client/src/slices/onlineSlice.js
@@ -0,0 +1,83 @@
+import { CUSTOMERS_URL } from "../../constants.js";
+import { apiSlice } from "./apiSlice.js";
+
+export const onlineSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    customerLogin: builder.mutation({
+      query: (data) => ({
+        url: `${CUSTOMERS_URL}/signin`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    customerRegister: builder.mutation({
+      query: (data) => ({
+        url: `${CUSTOMERS_URL}/signup`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    customerLogout: builder.mutation({
+      query: () => ({
+        url: `${CUSTOMERS_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+    CustomerProfile: builder.query({
+      query: (data) => ({
+        url: `${CUSTOMERS_URL}/getOnlineCustomerProfile`,
+        method: "GET",
+        body: data,
+      }),
+    }),
+    
+    CustomerProfileid: builder.query({
+        query: (userId) => ({
+          url: `${CUSTOMERS_URL}/${userId}`,
+          method: "GET",
+          body: data,
+        }),
+      }),
+  
+
+    getCustomer: builder.query({
+      query: () => ({
+        url: CUSTOMERS_URL,
+      }),
+      providesTags: ["Users"],
+      keepUnusedDataFor: 5,
+    }),
+    deleteCustomer: builder.mutation({
+      query: (userId) => ({
+        url: `${CUSTOMERS_URL}/${userId}`,
+        method: "DELETE",
+      }),
+    }),
+    getCustomerDetails: builder.query({
+      query: (userId) => ({
+        url: `${CUSTOMERS_URL}/${userId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    updateCustomer: builder.mutation({
+      query: (data) => ({
+        url: `${CUSTOMERS_URL}/${data.userId}`,
+        method: "PUT",
+        body: "data",
+      }),
+      invalidatesTags: ["Users"],
+    }),
+  }),
+});
+
+export const {
+  useCustomerLoginMutation,
+  useCustomerRegisterMutation,
+  useCustomerLogoutMutation,
+  useLazyCustomerProfileQuery,
+  useCustomerProfileidQuery,
+  useGetCustomerQuery,
+  useDeleteCustomerMutation,
+  useGetCustomerDetailsQuery,
+  useUpdateCustomerMutation,
+} = onlineSlice;
