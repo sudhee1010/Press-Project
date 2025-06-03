de5c6e8135ccea685d58de2f300f00640bebf130
sreebi <sreebisb@gmail.com>
2025-06-03 14:30:06 +0530

    notfound

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center justify-center text-sm h-[400px] text-center">
+      <p className="font-medium text-lg text-indigo-500">404 Error</p>
+      <h2 className="md:text-6xl text-4xl font-semibold text-gray-800">Page Not Found</h2>
+      <p className="text-base mt-4 text-gray-500">
+        Sorry, we couldn’t find the page you’re looking for.
+      </p>
+      <div className="flex items-center gap-4 mt-6">
+        <button
+          type="button"
+          onClick={() => navigate("/")}
+          className="bg-indigo-500 hover:bg-indigo-600 px-7 py-2.5 text-white rounded active:scale-95 transition-all"
+        >
+          Go back home
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
